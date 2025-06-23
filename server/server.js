30da2a7bd3e0ae64443852a49db071a3668b3fc8
Sumit Singh kaintura <98005540+sumitkainturaji@users.noreply.github.com>
2025-06-23 21:40:03 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(cors())
 app.use(clerkMiddleware())
 
 // Routes
-// app.get('/', (req, res) => res.send("API Working"))
+app.get('/', (req, res) => res.send("API Working"))
 app.post('/clerk', express.json() , clerkWebhooks)
 app.post('/stripe', express.raw({ type: 'application/json' }), stripeWebhooks)
 app.use('/api/educator', express.json(), educatorRouter)
